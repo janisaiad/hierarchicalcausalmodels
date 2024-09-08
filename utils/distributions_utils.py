586883db226d018e7261d2d441136b3236168381
janis <janisaiad.ja@gmail.com>
2024-09-08 18:04:38 +0200

    HCM for interv & resampling okay

--- utils/distributions_utils.py
+++ utils/distributions_utils.py
@@ -13,13 +13,14 @@ def create_subunit_distribution(data, node, unit_index, size):
     :param size: The number of subunits for this unit
     :return: A list of values for all subunits of this unit
     """
-    subunit_values = [data[f'_{node}{unit_index}_{k}'] for k in range(size[unit_index])]
+    subunit_values = [data[f'_{node}{unit_index}_{k}'] for k in range(size)]
     return subunit_values
 
 
 def ppf_functor(data, node, unit_index, size):
     # Convert data to a numpy array if it's not already
     data_array = create_subunit_distribution(data, node, unit_index, size[unit_index])
+    print(data_array, "data_array")
     x = np.random.random()
     return np.percentile(data_array, x * 100)
 
@@ -34,13 +35,14 @@ def cdf_functor(data):
 
 def pdf_functor(data):
     # Convert data to a numpy array if it's not already
-    data_array = np.array(list(data)) if isinstance(data, set) else np.array(data)
-    hist, bin_edges = np.histogram(data_array, bins='auto', density=True)
-    return lambda x: np.interp(x, (bin_edges[:-1] + bin_edges[1:]) / 2, hist)
+    #data_array = np.array(list(data)) if isinstance(data, set) else np.array(data)
+    #hist, bin_edges = np.histogram(data_array, bins='auto', density=True)
+    #return lambda x: np.interp(x, (bin_edges[:-1] + bin_edges[1:]) / 2, hist)
+    return lambda x: 1
 
 
-def distribution_functor(data, node, unit_index, size):
-    return ppf_functor(data, node, unit_index, size), cdf_functor(data), pdf_functor(data)
+def distribution_functor(data, node, unit_index, sizes):
+    return ppf_functor(data, node, unit_index, sizes), cdf_functor(data), pdf_functor(data)
 
 
 
