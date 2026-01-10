e57377ec5cea40bb3bda759f09402606db058fd0
janisaiad <janisaiad.ja@gmail.com>
2026-01-10 18:15:37 +0100

    added core files

---
+++ launch.sh
@@ -0,0 +1,10 @@
+curl -LsSf https://astral.sh/uv/install.sh
+pip install uv
+uv venv --clear
+source .venv/bin/activate
+cd src
+cd hierarchicalcausalmodels
+uv pip install -e .
+
+uv run tests/test_env.py
+source .venv/bin/activate
